40b96dc1cb6d97362739df3adfc1f5fbb8ff966f
Wilk323 <tdwilk44@rocketmail.com>
2014-10-22 12:07:30 -0400

    no message

--- model/Database_error.php
+++ model/Database_error.php
@@ -1,7 +1,7 @@
 <div id="main">
     <h1>Database Error</h1>
     <p>There was an error connecting to the database.</p>
-    <p>The database is not working properly .</p>
+    <p>The database is not working properly.</p>
     <p>Please see error message below</p>
     <p>Error message: <?php echo $error_message; ?></p>
     <p>&nbsp;</p>
