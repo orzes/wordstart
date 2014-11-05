a7d47c73da04dd5d18a1c3a799cd682bc0a88793
Aaron Rosa <a@foo.com>
2014-11-05 12:50:50 -0800

    added query

--- studentreportprocess.php
+++ studentreportprocess.php
@@ -1,22 +1,35 @@
 <?php
 
 
-$lesson_id = $_POST['lesson_id'];
-$student_id = $_POST['student_id'];
-$score_value = $_POST['lesson_score'];
 
 //print_r ($_POST);
 //exit();
 
-
+ $lesson_id = $_POST['lesson_id'];
+ $student_id = $_POST['student_id'];
+ $score_value = $_POST['lesson_score'];
+  
 // Update Score
-require_once('model/Database.php');
+require_once('Database.php');
+require_once('model/Score.php');
 
-$query= 'UPDATE scores SET lessonID = "'.$lesson_id.'", studentID = "'.$student_id.'", step_completed = "'.$score_value.'", 
-    WHERE lessonID = "'.$lesson_id.'" ';
+
+
+$score = new Score();
+
+$scoreResult=$score->updateScore($lesson_id, $student_id, $step_completed);
+
+//$sql= 'UPDATE scores SET lessonID = "'.$lesson_id.'", studentID = "'.$student_id.'", step_completed = "'.$score_value.'"  WHERE lessonID = "'.$lesson_id.'" ';
     
-print ($query);
+//print $sql;
+
+
 echo "Updating"
 
 
+
+   
+  
+
+ 
 ?>
